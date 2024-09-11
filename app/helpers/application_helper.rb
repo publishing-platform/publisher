1d218b038c29ecae260d72f95cf776cf69c28d1e
neilha <neilharrold@gmail.com>
2024-09-11 17:06:22 +0100

    adds navigation items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
   def name_or_fallback(user)
     user&.name || I18n.t("documents.unknown_user")
   end
+
+  def navigation_items
+    return [] unless current_user
+
+    items = []
+
+    items << { text: current_user.name, href: PublishingPlatformLocation.external_url_for("signon") }
+    items << { text: "Sign out", href: publishing_platform_sign_out_path }
+  end
+
+  def is_current?(link)
+    recognized = Rails.application.routes.recognize_path(link)
+    recognized[:controller] == params[:controller] &&
+      recognized[:action] == params[:action]
+  end
 end
