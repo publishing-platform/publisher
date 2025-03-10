2f848cc2dd243381007a6c7ee2db3f1314e1c7d6
neilha <neilharrold@gmail.com>
2025-03-10 21:40:35 +0000

    adds edition_url_helper spec

---
+++ spec/helpers/edition_url_helper_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe EditionUrlHelper do
+  let(:edition) do
+    build(:edition,
+          base_path: "/foo",
+          content_id: "d2547c42-8ed3-49f5-baeb-6112f98c2bf9")
+  end
+
+  describe "#edition_public_url" do
+    it "returns the URL" do
+      url = edition_public_url(edition)
+      expect(url).to eq("https://www.test.publishing-platform.co.uk/foo")
+    end
+
+    it "returns nil without a base_path" do
+      edition = build(:edition, base_path: nil)
+      url = edition_public_url(edition)
+      expect(url).to be_nil
+    end
+  end
+
+  describe "#edition_preview_url" do
+    it "returns the URL with an auth bypass token" do
+      allow(edition).to receive(:auth_bypass_token).and_return("secret")
+      url = edition_preview_url(edition)
+      expect(url).to eq("https://draft-origin.test.publishing-platform.co.uk/foo?token=secret")
+    end
+
+    it "returns nil without a base_path" do
+      edition = build(:edition, base_path: nil)
+      url = edition_preview_url(edition)
+      expect(url).to be_nil
+    end
+  end
+end
