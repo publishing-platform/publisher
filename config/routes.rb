bd256042b7feb1e2c79459411a57b168a9ea7c1e
neilha <neilharrold@gmail.com>
2025-03-20 09:22:10 +0000

    adds healthcheck route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
+
   root to: redirect("/documents")
 
   resources :documents, only: %i[index new create]
