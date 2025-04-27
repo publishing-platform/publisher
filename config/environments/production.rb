2927fbb23bbda6c1c34bc524aaf39fd09ff4efe7
neilha <neilharrold@gmail.com>
2025-04-27 21:47:45 +0100

    runs rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,11 +28,11 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Skip http-to-https redirect for the default health check endpoint.
-  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }  
-  
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
   # Log to STDOUT with the current request id as a default log tag.
-  config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.log_tags = [:request_id]
+  config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
@@ -85,10 +85,10 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_mailer.perform_caching = false
-    
+
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-    
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :terser
 
